b94a1aca7f3aa5d8ad84388e15e033214086e01d
jhengtan8 <evangeline.r.c.tan@accenture.com>
2025-11-26 05:58:20 +0000

    Fix indentation error for CORS middleware in settings.py

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -51,9 +51,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+
         # CORS middleware
         'corsheaders.middleware.CorsMiddleware',
+    ]
 
 ROOT_URLCONF = 'octofit_tracker.urls'
 
